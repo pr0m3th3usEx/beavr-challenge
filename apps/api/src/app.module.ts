16ab338a6ddd13e5b4c867e7ad326301811bb3e8
pr0m3th3usEx <thomas1.michel@epitech.eu>
2024-12-01 14:37:21 +0100

    add postgres adapter for database

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RequirementModule } from './requirements/requirement.module';
 import { DocumentModule } from './documents/document.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [RequirementModule, DocumentModule],
+  imports: [DatabaseModule, RequirementModule, DocumentModule],
 })
 export class AppModule {}
